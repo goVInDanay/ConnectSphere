7bae3adfbf493c3a57cc215edd0fd7affb894e47
Govind N Acharya <114073418+goVInDanay@users.noreply.github.com>
2026-05-18 10:15:22 +0530

    Delete CommentService directory

--- CommentService/src/main/java/com/connectsphere/commentservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.connectsphere.commentservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-}
