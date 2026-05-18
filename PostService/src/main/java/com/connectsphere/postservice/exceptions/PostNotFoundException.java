91f48ef17e5b1b0fa85dd0de26d0afafc447907c
Govind N Acharya <114073418+goVInDanay@users.noreply.github.com>
2026-05-18 10:15:38 +0530

    Delete PostService directory

--- PostService/src/main/java/com/connectsphere/postservice/exceptions/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.connectsphere.postservice.exceptions;
-
-public class PostNotFoundException extends RuntimeException {
-	public PostNotFoundException(String message) {
-		super(message);
-	}
-}
